4b9b74a1a802b4c6503640092860de98efe01ae6
muizat bakri <you@example.com>
2026-08-06 23:12:24 +0100

    fix: correct sidebar import casing in Dashboard.jsx

--- src/Pages/dashboard/Dashboard.jsx
+++ src/Pages/dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import Sidebar from "../../components/Sidebar/Sidebar";
+import  Sidebar from "../../components/sidebar/Sidebar"
 import styles from "./dashboard.module.css";
 
 const Dashboard = () => {
